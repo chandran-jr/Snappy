69e6f6d9ab88d05324b63749ec32f5e664c8b31a
Govind B Chandran <govindchandran235@gmail.com>
2020-08-15 12:18:02 +0530

    Add files via upload

---
+++ go/conversion.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int
+	i=4
+	var j float32
+  j = float32(i)
+	fmt.Println(i)
+	fmt.Println(j)
+}
---
+++ go/variables.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var i int
+	i=4
+	j:=10
+	fmt.Println(i)
+	fmt.Println(j)
+}
