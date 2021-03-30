a205a0071c7480fc95dc8f9872b4b6c4f19fc165
Govind B Chandran <govindchandran150@gmail.com>
2021-03-30 10:28:49 +0530

    Delete dart directory

--- dart/5_logicalOp.dart
+++
@@ -1,7 +0,0 @@
-void main(){
-  int a = 12;
-  int b = 5;
-
-  print(a is int && b is! int);
-  print(a is int || b is! int);
-}
--- dart/8_forIn.dart
+++
@@ -1,8 +0,0 @@
-void main(){
-
-  var data = [12, 13, 14]; 
-  for (var i in data){ 
-    print(i);
-  } 
-
-}
