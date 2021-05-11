dab2e8cd0ab53fd8e464461f75c1fce3f49ec6b6
Govind B Chandran <govindchandran235@gmail.com>
2021-05-12 00:03:56 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './App.css';
 import WebcamCapture from './WebcamCapture';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <WebcamCapture/>
+
+
+<Router>
+      <div className="app__body">
+        <Switch>
+          <Route exact path="/">
+          <WebcamCapture/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </div>
   );
 }
