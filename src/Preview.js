c0c08dd05731c3c7f551837eaf32871129bb91e7
Govind B Chandran <govindchandran235@gmail.com>
2021-05-13 11:51:48 +0530

    Add files via upload

--- src/Preview.js
+++ src/Preview.js
@@ -3,6 +3,13 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { resetCameraImage, selectCameraImage } from './features/cameraSlice';
 import CloseIcon from '@material-ui/icons/Close';
+import TextFieldsIcon from '@material-ui/icons/TextFields';
+import CreateIcon from '@material-ui/icons/Create';
+import NoteIcon from '@material-ui/icons/Note';
+import MusicNoteIcon from '@material-ui/icons/MusicNote';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import CropIcon from '@material-ui/icons/Crop';
+import TimerIcon from '@material-ui/icons/Timer';
 import './preview.css';
 
 function Preview() {
@@ -27,6 +34,17 @@ function Preview() {
          className="preview__close"
          onClick={closePreview}    
          />
+
+         <div className="preview__toolbarRight">   
+         <TextFieldsIcon/>
+            <CreateIcon/>
+            <NoteIcon/>
+            <MusicNoteIcon/>
+            <AttachFileIcon/>
+            <CropIcon/>
+            <TimerIcon/>
+         </div>
+
             <img src={cameraImage} alt=""/>
         </div>
     );
