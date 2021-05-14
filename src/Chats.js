90121f171ba4e1fd1bb3d4c61d439ac1cd6b5b16
Govind B Chandran <govindchandran235@gmail.com>
2021-05-14 14:34:54 +0530

    Add files via upload

--- src/Chats.js
+++ src/Chats.js
@@ -1,10 +1,20 @@
+import { Avatar } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 import React from 'react';
 import './Chats.css';
 
 function Chats() {
     return (
         <div className="chats">
-            
+            <div className="chats__header">
+                <Avatar className="chats__avatar"/>
+                <div className="chats__searchbar">
+                    <SearchIcon className="chats__searchIcon" />
+                    <input type="text" placeholder="Search" />
+                </div>
+                <ChatBubbleIcon className="chats__chatBubbleIcon" />
+            </div>
         </div>
     )
 }
