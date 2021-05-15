424265228a0f0e78f589fb97f9510907a13ef917
Govind B Chandran <govindchandran235@gmail.com>
2021-05-15 19:52:02 +0530

    Add files via upload

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './Chat.css';
 
-function Chat() {
+function Chat({id,timestamp,username, profilePic, read, imageUrl}) {
     return (
-        <div>
-            
+        <div className="chat">
+            <Avatar src={profilePic} />
+            <div className="chat__info">
+                <h4>Username</h4>
+                <p>Tap to view ~ {new Date(timestamp?.toDate()).toUTCString()} </p>
+            </div>
         </div>
     )
 }
