c3cf2e8fd4a980da9f9ae6e2cb0439961d026493
Govind B Chandran <govindchandran235@gmail.com>
2021-05-17 11:25:02 +0530

    Add files via upload

--- src/Chat.js
+++ src/Chat.js
@@ -1,6 +1,7 @@
 import { Avatar } from '@material-ui/core';
 import StopIcon from '@material-ui/icons/Stop';
 import React from 'react';
+import ReactTimeago from 'react-timeago';
 import './Chat.css';
 
 function Chat({id,timestamp,username, profilePic, read, imageUrl}) {
@@ -9,7 +10,7 @@ function Chat({id,timestamp,username, profilePic, read, imageUrl}) {
             <Avatar src={profilePic} />
             <div className="chat__info">
                 <h4>Username</h4>
-                <p>Tap to view ~ {new Date(timestamp?.toDate()).toUTCString()} </p>
+                <p>Tap to view ~ <ReactTimeago date = {new Date(timestamp?.toDate()).toUTCString()} /> </p>
             </div>
             {!read && <StopIcon className="chat__stopicon"/>}
         </div>
